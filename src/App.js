010ee4dbd0e260b68184d2e1a6326c97e910bbce
Pinja Jäkkö <pinja@jakko.fi>
2020-11-27 16:45:00 +0200

    Add availability info

--- src/App.js
+++ src/App.js
@@ -1,44 +1,76 @@
-import { React, useEffect, useState, useMemo } from "react";
+import { React, useState, useMemo, useEffect } from "react";
 import { List } from "react-virtualized";
 
 import Item from "./Item";
 
 const App = () => {
-  const [selected, setSelected] = useState("accessories");
-  const [accessories, setAccessories] = useState([]);
-  const [jackets, setJackets] = useState([]);
-  const [shirts, setShirts] = useState([]);
+  const categories = useMemo(() => ["accessories", "jackets", "shirts"], []);
+  const [selected, setSelected] = useState(categories[0]);
+  const [items, setItems] = useState({});
+  const [availability, setAvailability] = useState({});
+  const [loading, setLoading] = useState(true);
 
-  const items = { accessories, jackets, shirts };
+  useEffect(() => {
+    const api_url = "https://bad-api-assignment.reaktor.com";
+    Promise.all(
+      categories.map((category) => {
+        return fetch(`${api_url}/products/${category}`)
+          .then((res) => res.json())
+          .then((res) => ({ [category]: res }));
+      })
+    )
+      .then((content) => {
+        const items = content.reduce(
+          (prev, current) => ({ ...prev, ...current }),
+          {}
+        );
+        setItems(items);
+        return new Set(
+          []
+            .concat(...content.map((object) => Object.values(object)[0]))
+            .map((item) => item.manufacturer)
+        );
+      })
+      .then((manufacturers) => {
+        Promise.all(
+          [...manufacturers].map((manufacturer) => {
+            return fetch(`${api_url}/availability/${manufacturer}`)
+              .then((res) => res.json())
+              .then((res) => res.response);
+          })
+        ).then((responses) => {
+          const availability = {};
+          const parser = new DOMParser();
+          [].concat(...responses).forEach((item) => {
+            const xmlDoc = parser.parseFromString(item.DATAPAYLOAD, "text/xml");
+            availability[item.id.toLowerCase()] =
+              xmlDoc.getElementsByTagName("INSTOCKVALUE")[0].innerHTML ===
+              "INSTOCK";
+          });
+          setAvailability(availability);
+          setLoading(false);
+        });
+      });
+  }, [categories, setLoading, setItems, setAvailability]);
 
   const select = (category) => () => {
     setSelected(category);
   };
 
-  const categories = useMemo(() => ["accessories", "jackets", "shirts"], []);
-
-  useEffect(() => {
-    const api_url = "https://bad-api-assignment.reaktor.com";
-
-    const setters = {
-      accessories: setAccessories,
-      jackets: setJackets,
-      shirts: setShirts,
-    };
-
-    const fetchCategory = (category) => {
-      fetch(`${api_url}/products/${category}`)
-        .then((res) => res.json())
-        .then((content) => setters[category](content));
-    };
-    for (const category of categories) fetchCategory(category);
-  }, [setSelected, setAccessories, setJackets, categories]);
-
-  const renderRow = ({ index, key, style }) => (
-    <Item key={key} style={style} {...items[selected][index]} />
-  );
-
-  return (
+  const renderRow = ({ index, key, style }) => {
+    const item = items[selected][index];
+    return (
+      <Item
+        key={key}
+        style={style}
+        {...items[selected][index]}
+        inStock={availability[item.id]}
+      />
+    );
+  };
+  return loading ? (
+    <div>Loading data from server</div>
+  ) : (
     <>
       {categories.map((category) => (
         <button key={category} onClick={select(category)}>
--- src/Item.js
+++ src/Item.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export const Item = ({ style, name, color, price, manufacturer }) => {
+export const Item = ({ style, name, color, price, manufacturer, inStock }) => {
   return (
     <div style={style}>
       <div style={{ fontWeight: "bold" }}>{name}</div>
       <div>Colors: {color.join(", ")}</div>
       <div>{price}€</div>
       <div>Manufactured by {manufacturer}</div>
+      <div>{inStock ? "in stock" : "out of stock"}</div>
     </div>
   );
 };
